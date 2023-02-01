ffa2bc6cf77b6a010a53962d3bb51519221a5cb1
Mantrel <109878997+Mantrel@users.noreply.github.com>
2023-02-01 20:32:16 -0300

    Update Defecto.jsx

--- src/views/Defecto.jsx
+++ src/views/Defecto.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Defecto = () => {
   return (
     <div>
-        <center className='pt-5'><h1>Lo siento, aún estamo horneando pasteles por aquí 🎂👨‍🍳</h1></center>
+        <center className='pt-5'><h1>Lo siento, aún estamos horneando pasteles por aquí 🎂👨‍🍳</h1></center>
     </div>
   )
 }
